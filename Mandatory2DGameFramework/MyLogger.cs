1c1c67b692950c5557f9f1abdefe87ad7b26b091
Glenning <glenning.thor@gmail.com>
2024-10-03 11:39:12 +0200

    Add project files.

---
+++ Mandatory2DGameFramework/MyLogger.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mandatory2DGameFramework
+{
+    public class MyLogger
+    {
+        private const string logname = "GAMELOG";
+
+        public void Start()
+        {
+            TraceSource tSource = new TraceSource(logname);
+            tSource.Switch = new SourceSwitch(logname, SourceLevels.Information.ToString());
+
+            tSource.Listeners.Add(new ConsoleTraceListener());
+
+            tSource.Listeners.Add(
+                new TextWriterTraceListener($"{logname}.txt")
+                {
+                    Filter = new EventTypeFilter(SourceLevels.Error)
+                });
+
+            tSource.Listeners.Add(new XmlWriterTraceListener($"{logname}.xml"));
+
+            tSource.TraceEvent(TraceEventType.Information, 700, "Message: Something went wrong");
+            tSource.Close();
+        }
+    }
+}
