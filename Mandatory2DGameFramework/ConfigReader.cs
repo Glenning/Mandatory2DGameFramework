3a21f80f26dfb9a16313f359e2e6bf924181773f
Glenning <glenning.thor@gmail.com>
2024-10-21 14:11:54 +0200

    Added xml reader (not done)

---
+++ Mandatory2DGameFramework/ConfigReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Mandatory2DGameFramework
+{
+    public class ConfigReader
+    {
+        public void Read(string filename)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(filename);
+        }
+    }
+}
