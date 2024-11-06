6aa4ee7fd8ca3b1306d9a2568addfebacb460f55
Glenning <glenning.thor@gmail.com>
2024-11-06 19:20:35 +0100

    Added a test project

---
+++ TestGame/Program.cs
@@ -0,0 +1,9 @@
+﻿using Mandatory2DGameFramework;
+using Mandatory2DGameFramework.model.Creatures;
+using Mandatory2DGameFramework.model.Cretures;
+
+Console.WriteLine("This is a test environment ");
+
+Creature Warrior = CreatureFactory.MakeCreature(CreatureType.Warrior);
+
+Console.WriteLine(Warrior);
